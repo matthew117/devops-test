d53f16bad5a752ac139f100ab628b1e9603e34d4
Stefan Gavrilas <stefan.gabriel.gavrilas@gmail.com>
2021-02-04 15:24:51 +0000

    Initial commit.

---
+++ Controllers/DevOpsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/devops")]
+    public class DevOpsController : ControllerBase
+    {
+        [HttpGet("build/{number}")]
+        public IActionResult GetBuildNumber(string number)
+        {
+            return Ok($"Secret is : {ServerData.Secret}. Build number is : {number}");
+        }
+    }
+}
---
+++ RandomConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApi
+{
+    public class RandomConfig
+    {
+        public Secret Secret { get; set; }
+    }
+
+    public class Secret
+    {
+        public string Value { get; set; }
+    }
+}
